a166bd61cd02dbf3db2e974bcc8c7e034b91ecac
Jingcheng Wang <jingwang49@gmail.com>
2024-02-08 09:27:19 +0800

    script buttons and inputs

---
+++ Projects/JS/TextToSpeech/script.js
@@ -0,0 +1,5 @@
+const playButton = document.getElementById('play-button')
+const pauseButton = document.getElementById('pause-button')
+const stopButton = document.getElementById('stop-button')
+const textInput = document.getElementById('text')
+const speedInput = document.getElementById('speed')
